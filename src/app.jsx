e976bd5785a88ce20b3445bab4e27701e5095af7
Mattias <66011769+SmackDH@users.noreply.github.com>
2022-04-22 14:13:38 +0900

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./heading.jsx";
+import List from "./list";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
